1d287b21dee4277fcc9eda10ccd5a13d6935d182
520work <1975989325@qq.com>
2018-01-18 16:27:44 +0800

    Initial commit

---
+++ data.js
@@ -0,0 +1,120 @@
+[{
+    "createdTime": "2017-12-1",
+    "createdUserID": 1,
+    "isLogin": 1,
+    "lastModifyUserID": 1,
+    "lastModifyedTime": "1",
+    "loginTime": "1",
+    "passwordState": 1,
+    "pwdSeed": "1",
+    "roleAdditional": "1",
+    "roleGroup": "1",
+    "securityProtection": {
+        "bindNoticeFlag": 3,
+        "createdTime": "3",
+        "seqNo": 1,
+        "serviceUser": {
+            "$ref": ".."
+        },
+        "spEmail": "3",
+        "spEmailState": 3,
+        "spMobile": "18743260935",
+        "spMobileState": 3
+    },
+    "seqNo": 1,
+    "service": {
+        "serviceID": 1,
+        "serviceNo": "112313",
+        "serviceUser": {
+            "$ref": ".."
+        }
+    },
+    "serviceAccount": {
+        "accountType": "2",
+        "allBorrowedMoney": 2,
+        "allRetPoint": 2,
+        "allRetProfit": 2,
+        "allShipppedMoney": 2,
+        "allUserPaid": 2,
+        "balance": 2,
+        "costType": 2,
+        "createTime": "2",
+        "endDate": "2",
+        "flag": "2",
+        "haveTerm": "2",
+        "lastRetProfit": 2,
+        "modifyedTime": "2",
+        "nickname": "2",
+        "percents": "2",
+        "serviceAccountID": 1,
+        "serviceID": 2,
+        "serviceUser": {
+            "$ref": ".."
+        },
+        "state": 2,
+        "unRetBorrowedMoney": 2
+    },
+    "serviceID": 1,
+    "state": 1,
+    "userAccount": "1111",
+    "userName": "老大",
+    "userPassword": "3333",
+    "userPhone": "1111"
+}];
+
+[{
+    "createdTime": "2017-12-5",
+    "createdUserID": 2,
+    "isLogin": 2,
+    "lastModifyUserID": 2,
+    "lastModifyedTime": "2",
+    "loginTime": "2",
+    "passwordState": 2,
+    "pwdSeed": "2",
+    "roleAdditional": "2",
+    "roleGroup": "2",
+    "securityProtection": {
+        "bindNoticeFlag": 334535,
+        "createdTime": "3435354",
+        "seqNo": 2,
+        "serviceUser": { "$ref": ".." },
+        "spEmail": "345453",
+        "spEmailState": 34535,
+        "spMobileState": 453
+    },
+    "seqNo": 2,
+    "service": {
+        "serviceID": 2,
+        "serviceNo": "44424",
+        "serviceUser": { "$ref": ".." }
+    },
+    "serviceAccount": {
+        "accountType": "785",
+        "allBorrowedMoney": 785,
+        "allRetPoint": 57,
+        "allRetProfit": 875,
+        "allShipppedMoney": 785,
+        "allUserPaid": 578,
+        "balance": 758,
+        "costType": 587,
+        "createTime": "578",
+        "endDate": "785",
+        "flag": "875",
+        "haveTerm": "587",
+        "lastRetProfit": 785,
+        "modifyedTime": "587",
+        "nickname": "578",
+        "percents": "785",
+        "serviceAccountID": 2,
+        "serviceID": 785,
+        "serviceUser": { "$ref": ".." },
+        "state": 785,
+        "unRetBorrowedMoney": 578
+    },
+    "serviceID": 2,
+    "state": 2,
+    "userAccount": "2222",
+    "userName": "老二",
+    "userPassword": "4444",
+    "userPhone": "1234"
+}]
